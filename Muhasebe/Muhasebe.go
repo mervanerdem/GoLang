8fa067870779fa2019a39b4a4848506c893a421f
Mervan ERDEM <mrvnerdem@gmail.com>
2022-02-15 22:33:34 +0300

    First Commit

---
+++ Muhasebe/Muhasebe.go
@@ -0,0 +1,17 @@
+//Muhassebe Programı - Accounting Program
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	var sname string
+	fmt.Println("What's your Name: ")
+	fmt.Scan(&name)
+	fmt.Println("Your Surname: ")
+	fmt.Scan(&sname)
+	fmt.Println("Your Full Name: ")
+	fmt.Println(name + " " + sname)
+
+}
