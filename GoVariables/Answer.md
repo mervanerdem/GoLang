e8e82566a0a96de6d29d76db80323fac5b991169
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-02-24 16:37:13 +0300

    You Can Do It Camp

--- GoVariables/Answer.md
+++ GoVariables/Answer.md
@@ -1,3 +1,6 @@
-Answer
+Output
     
     50
+    Hello Word!
+    15
+    1 2 3
--- GoVariables/FirstValue.go
+++ GoVariables/FirstValue.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var myNum = 50
+	var myNum = 50 //Take int automaticly
 	fmt.Println(myNum)
 
+	var myWord = "Hello Word!" //Take int automaticly
+	fmt.Println(myWord)
+
+	var x = 5
+	var y = 10
+	var z = x + y
+	fmt.Println(z)
+
+	var a, b, c = 1, 2, 3
+	fmt.Println(a, b, c)
 }
