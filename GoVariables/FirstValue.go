fd43aa83b3ded51234a25bf74137fec7f5be36a6
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-02-24 16:40:11 +0300

    You Can Do It Camp

--- GoVariables/FirstValue.go
+++ GoVariables/FirstValue.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var myNum = 50 //Take int automaticly
-	fmt.Println(myNum)
+	var myNum = 50     //Take int automaticly
+	fmt.Println(myNum) //Output
 
 	var myWord = "Hello Word!" //Take int automaticly
-	fmt.Println(myWord)
+	fmt.Println(myWord)        //Output
 
-	var x = 5
-	var y = 10
-	var z = x + y
-	fmt.Println(z)
+	var x = 5      // int number x = 5
+	var y = 10     //int number x = 5
+	var z = x + y  // mathematical operation used
+	fmt.Println(z) //Output
 
-	var a, b, c = 1, 2, 3
-	fmt.Println(a, b, c)
+	var a, b, c = 1, 2, 3 //You can use this method sameway
+	fmt.Println(a, b, c)  //Output
 }
