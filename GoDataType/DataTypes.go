549bbea815129a7e4bed890a719447eb1faa6d53
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-02-24 16:48:46 +0300

    You Can Do It Champ

--- GoDataType/DataTypes.go
+++ GoDataType/DataTypes.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var myNum int = 123
-	var myWord string = "Hi!"
-	var myBool bool = true
+	var myNum int = 123       //int number
+	var myWord string = "Hi!" //string
+	var myBool bool = true    //Just true or false
 
 	fmt.Println(myNum, myWord, myBool)
 
 }
+
+/* Output
+123 Hi! true
+*/
