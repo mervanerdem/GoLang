b2dc6f712cab2db281818999bc2819423325dbfd
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-02 20:50:01 +0300

    You Can Do Champ

--- 03-GoVariables/FirstValue.go
+++ 03-GoVariables/FirstValue.go
@@ -9,9 +9,9 @@ func main() {
 	var myWord = "Hello Word!" //Take int automaticly
 	fmt.Println(myWord)        //Output
 
-	var x = 5      // int number x = 5
-	var y = 10     //int number x = 5
-	var z = x + y  // mathematical operation used
+	var x int = 5  // int number x = 5
+	var y = 10     //int number y = 5
+	z := x + y     // mathematical operation used and int number z
 	fmt.Println(z) //Output
 
 	var a, b, c = 1, 2, 3 //You can use this method sameway!
