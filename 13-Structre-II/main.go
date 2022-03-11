8ac137c9e5a87c898b11ac4dbde5e9450b42d740
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-12 00:54:19 +0300

    Is a Relation/ Has a relation/Anonim Struct

--- 13-Structre-II/main.go
+++ 13-Structre-II/main.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
-type employee struct { // veri tip haline dönüştürerek defalarca struct yaratmaktan kaçınılmaktadır.
-	name      string //Aynı zamanda Underlying type diye de geçer
-	age       int    //main fonksiyonun içinde de tanımlanabilir
+type employee struct { //Is a Relation Kullanılır:
+	name      string //Ahmet ile mehmet işçidir. fakat ahmetle mehmet aynı değildir.
+	age       int    //Ya da Aslan ile Kaplan hayvandır hayvan özelliği taşır ama aynı değiller
 	isMarried bool
+}
+
+type manager struct { //Has a relation kullanılır:
+	employee          //Manager bir employe değildir sadece bazı özelliklerini taşır.
+	profession string //mantar bir bitki değildir ama bitkilerin bazı özelliklerini taşır.
 }
 
 func main() {
@@ -14,11 +19,38 @@ func main() {
 		age:       46,
 		isMarried: true,
 	}
-	fmt.Println(e1)
+
+	/* fmt.Println(e1)
 	e2 := e1
 	e2.name = "Selami" //e1 değerini değiştirmez.
-
 	fmt.Println(e1)
 	fmt.Println(e2)
+	*/
+
+	m1 := manager{
+		employee: employee{
+			name:      "Mervan",
+			age:       24,
+			isMarried: false,
+		},
+		profession: "Engineer",
+	}
+
+	m2 := manager{} // Farklı bir tanımlama şekli
+	m2.name = "Salih"
+	m2.age = 38
+	m2.isMarried = true
+	m2.profession = "Buisness Manager"
+
+	fmt.Println(e1)
+	fmt.Println(m1)
+	fmt.Println(m2)
+
+	CEO := struct { //Anonim Struct yapısı olarak geçer.
+		name  string //Birden çok kez kullanılmayacaksa fonksiyon içerisinde kullanılır. Global değişken yapmaya gerek yoktur.
+		money int
+	}{name: "Cevdet", money: 120000}
+
+	fmt.Println(CEO)
 
 }
