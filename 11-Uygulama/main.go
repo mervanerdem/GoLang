53e3a315e83803594c4d4ce60e198221d834150d
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-11 18:51:56 +0300

    Örnekler çözüldü.

---
+++ 11-Uygulama/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		//1- 6 sting karekterden oluşan bir array ve 5 int elemandan oluşan slice oluşturup index numaralarıyla birlikte gösterelim.
+		myArr := [6]string{"Ben", "Sen", "O", "Biz", "Siz", "Onlar"} //Array kısmı
+		for index, value := range myArr {
+			fmt.Println(index, value)
+		}
+
+		fmt.Println("------------------------")
+
+		mySlc := []int{100, 200, 300, 400, 500}
+		for i, v := range mySlc {
+			fmt.Println(i, v)
+		} */
+
+	/* 	//2-[0,1,2,3,4,5,6,7,8] slice'ından [0,1,2,3] ,[4,5,6] , [6,7,8], [2,3,6,7] slice'larını oluşturunuz.
+
+	   	mySlc := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	   	mySlc2 := mySlc[:4]
+	   	mySlc3 := mySlc[4:7]
+	   	mySlc4 := mySlc[6:]
+	   	mySlc5 := append(mySlc[2:4], mySlc[6:8]...)
+
+	   	fmt.Println("My Slc", mySlc)
+	   	fmt.Println("My Slc2", mySlc2)
+	   	fmt.Println("My Slc3", mySlc3)
+	   	fmt.Println("My Slc4", mySlc4)
+	   	fmt.Println("My Slc5", mySlc5)
+	*/
+	//3-slicelar için copy metodu ve assign(=) farkını açıklayın.
+	mySlc := []int{0, 1, 2}
+	mySlc2 := make([]int, 2)
+	mySlc3 := make([]int, 3)
+
+	fmt.Println(mySlc)
+	fmt.Println(mySlc2)
+	fmt.Println(mySlc3)
+
+	fmt.Println("-----------")
+
+	copy(mySlc2, mySlc) //copy
+	mySlc3 = mySlc      //append (=)
+
+	fmt.Println(mySlc)
+	fmt.Println(mySlc2)
+	fmt.Println(mySlc3)
+
+	fmt.Println("-----------")
+
+	mySlc2[1] = 3 //kopyalanan
+	mySlc3[0] = 7 // atanan
+
+	fmt.Println(mySlc)
+	fmt.Println(mySlc2)
+	fmt.Println(mySlc3)
+
+}
