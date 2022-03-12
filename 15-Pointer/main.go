d5deca6b6b2155e9b0b1783f49161132b9786a76
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-12 19:29:41 +0300

    pointer adresinin gösterdiği değer ile adresin dönüşümü yapıldı.

--- 15-Pointer/main.go
+++ 15-Pointer/main.go
@@ -9,10 +9,16 @@ func main() {
 	   	fmt.Println(&name) //& --->pointer adres gösterici(Address Operator) */
 
 	x := 15
-	fmt.Printf("%T  %v\n", x, x)   //%T veri tipini, \n alt satıra geçişi ve %v değerini ifade etmektedir.
-	fmt.Printf("%T  %v\n", &x, &x) //pointer adreslerini görüntüleme
+	/* 	fmt.Printf("%T  %v\n", x, x)   //%T veri tipini, \n alt satıra geçişi ve %v değerini ifade etmektedir.
+	   	fmt.Printf("%T  %v\n", &x, &x) //pointer adreslerini görüntüleme
 
-	y := &x                      //y pointer olarak tanımlanıp x'in adresi atandı.
-	fmt.Printf("%T  %v\n", y, y) //y *int değişkenine sahip
+	   	y := &x                      //y pointer olarak tanımlanıp x'in adresi atandı.
+	   	fmt.Printf("%T  %v\n", y, y) //y *int değişkenine sahip */
+
+	fmt.Println(x)        // x değeri
+	fmt.Println(&x)       //x'in adresi
+	fmt.Println(*(&x))    //x'in adresinin gösterdiği değer-> yani x
+	fmt.Println(&(*(&x))) // //x'in adresinin gösterdiği değerin adresi yani-> x'in adresi
+	// * adresteki değeri gösterir, & değerin adresini gösterir. Yani birbirine dönüşüm yapılabilir.
 
 }
