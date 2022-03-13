a5d5cd95db3d917e928de9061aa54abe7d3a5753
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-13 18:03:20 +0300

    Pointer Fonksiyonu ile işlem yapıldı.

--- 16-Pointer-II/main.go
+++ 16-Pointer-II/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x := 5                      //Pass by value
+	/* x := 5                      //Pass by value
 	slc := []int{0, 1, 2}       //Pass by referances
 	myArr := [4]int{0, 1, 2, 3} //Pass by value
 	fmt.Println("main1:", x)
@@ -14,10 +14,16 @@ func main() {
 	doubleArr(myArr)
 	fmt.Println("main2:", x)     //x'in değişmemesinin sebebi değeri fonksiyona kopyalanır.
 	fmt.Println("main2:", slc)   //slc'in referans adresi kopyalanır. Slice'ın pointer özelliğinden dolayı.
-	fmt.Println("main2:", myArr) // Array'in değeri kopyalanır. O yüzden değer değişmişyor.
+	fmt.Println("main2:", myArr) // Array'in değeri kopyalanır. O yüzden değer değişmişyor. */
+
+	y := 8
+	fmt.Println("main1:", y)
+	doublePoi(&y)            //Adresi fonksiyona gönderildi.
+	fmt.Println("main2:", y) //adresindeki değeri fonksiyonda 2ile çarptığımız için ana fonksitondaki da değişir.
+
 }
 
-func double(x int) {
+/* func double(x int) {
 	x = x * 2
 	fmt.Println("Fonksiyon:", x)
 }
@@ -34,4 +40,9 @@ func doubleArr(myArr [4]int) {
 		myArr[i] *= 2
 	}
 	fmt.Println("Fonksiyon:", myArr)
+} */
+
+func doublePoi(y *int) { //Pointer method
+	*y *= 2 //adresindeki değeri 2 ile çarpıyoruz.
+	fmt.Println("Fonksiyon:", *y)
 }
