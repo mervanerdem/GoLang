c503e3a7d78151669300f9673b3a98bc29c85f40
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-13 22:28:03 +0300

    Oluşturduğumuz değişkenle ilgili örnek çözüldü.

--- 17-Uygulma-III/main.go
+++ 17-Uygulma-III/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println(x)
 } */
 
-//Başlangıç değeri 10 olan bir x değeri atayın.
+/* //2-Başlangıç değeri 10 olan bir x değeri atayın.
 //Bu x'in adresini y'ye tanımlayıp x değerini 20 diye güncelleyin.
 
 package main
@@ -25,3 +25,38 @@ func main() {
 	*y = 20
 	fmt.Println(x)
 }
+*/
+//3-Underlying Type struct olan Rectangle type oluşturunuz.
+//display,area,circumference metodlarını yazınız.
+
+package main
+
+import "fmt"
+
+type rectangle struct {
+	a, b int
+}
+
+func (r rectangle) display() {
+	fmt.Println("Kenar uzunlukları:", r.a, "ve", r.b, "olan dikdörtgenin")
+}
+
+func (r rectangle) area() int {
+	return r.a * r.b
+
+}
+
+func (r rectangle) circumference() int {
+	return 2 * (r.a + r.b)
+
+}
+
+func main() {
+	r1 := rectangle{5, 8}
+	r1.display()
+	fmt.Println("Alanı:", r1.area())
+	fmt.Println("Çevresi:", r1.circumference())
+}
+
+//4- 4 adet user'ı struct yapısıyla farklı şekilde tanımlayınız.
+//name, age, int -> For döngüsüyle kullanıcıları gösteriniz.
