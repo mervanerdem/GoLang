2d466036e433c7c9b688d7ee6680368080527158
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-13 19:48:55 +0300

    Pointer üzerinden değer güncelleme

--- 17-Uygulma-III/main.go
+++ 17-Uygulma-III/main.go
@@ -1,7 +1,7 @@
 //1-Underlying type 'int' olacak şekilde kendi veri tipinizi oluşturunuz
 //veri tipi değerini '10' yazdırınız.
 
-package main
+/* package main
 
 import "fmt"
 
@@ -9,5 +9,19 @@ type sayi int
 
 func main() {
 	var x sayi = 10
+	fmt.Println(x)
+} */
+
+//Başlangıç değeri 10 olan bir x değeri atayın.
+//Bu x'in adresini y'ye tanımlayıp x değerini 20 diye güncelleyin.
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	y := &x
+	*y = 20
 	fmt.Println(x)
 }
