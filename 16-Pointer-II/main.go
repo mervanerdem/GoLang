9c7eabf77311d69dfb7b17aef1a29603c4310362
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-13 17:56:43 +0300

    Temel olarak değerler üstündeki etkisi incelendi.

---
+++ 16-Pointer-II/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 5                      //Pass by value
+	slc := []int{0, 1, 2}       //Pass by referances
+	myArr := [4]int{0, 1, 2, 3} //Pass by value
+	fmt.Println("main1:", x)
+	fmt.Println("main1:", slc)
+	fmt.Println("main1:", myArr)
+	double(x)
+	doubleSlc(slc)
+	doubleArr(myArr)
+	fmt.Println("main2:", x)     //x'in değişmemesinin sebebi değeri fonksiyona kopyalanır.
+	fmt.Println("main2:", slc)   //slc'in referans adresi kopyalanır. Slice'ın pointer özelliğinden dolayı.
+	fmt.Println("main2:", myArr) // Array'in değeri kopyalanır. O yüzden değer değişmişyor.
+}
+
+func double(x int) {
+	x = x * 2
+	fmt.Println("Fonksiyon:", x)
+}
+
+func doubleSlc(slc []int) {
+	for i := 0; i < len(slc); i++ {
+		slc[i] *= 2
+	}
+	fmt.Println("Fonksiyon:", slc)
+}
+
+func doubleArr(myArr [4]int) {
+	for i := 0; i < len(myArr); i++ {
+		myArr[i] *= 2
+	}
+	fmt.Println("Fonksiyon:", myArr)
+}
