bdfca74ed893824a3722bde96893095dfa1adb6b
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-15 00:24:51 +0300

    Channel nasıl kullanılmaz örneği.

--- 21-Channels/main.go
+++ 21-Channels/main.go
@@ -1,16 +1,29 @@
+/* package main
+
+import "fmt"
+
+func main() {
+	// 	var myChannel chan string
+	 //  	myChannel = make(chan string) //Uzun yöntem channel tanımlama
+
+	myChannel := make(chan string) //Kısa yol channel tanımlama
+	go merhaba(myChannel)          //myChannel değişkeni GoRoutine ile gönderildi.
+
+	fmt.Println(<-myChannel) //burda da döndürdüğümüz değeri okutabiliyoruz.
+}
+func merhaba(channel chan string) {
+	channel <- "Merhaba" //Buradan ok operatörü ile return yerine fonksiyondan geri dönüş sağladık.
+} */
+
+//Burada hata alacağız
 package main
 
 import "fmt"
 
 func main() {
-	/* 	var myChannel chan string
-	   	myChannel = make(chan string) //Uzun yöntem channel tanımlama
-	*/
-	myChannel := make(chan string) //Kısa yol channel tanımlama
-	go merhaba(myChannel)
+	chan1 := make(chan string)
 
-	fmt.Println(<-myChannel)
-}
-func merhaba(channel chan string) {
-	channel <- "Merhaba"
+	chan1 <- "Merhaba" //değer kanala gönderildi. başka bir go routine gönderilmek adına
+
+	fmt.Println(<-chan1) // fakat başka bir goroutineden geri dönüş olmadığından foksiyon hataya düşmekte.
 }
