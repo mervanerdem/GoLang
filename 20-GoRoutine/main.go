0f90f4a32d22f28fe246766136958d562e261879
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-14 20:46:45 +0300

    Efektif olmayan bir GoRoutine örneği

--- 20-GoRoutine/main.go
+++ 20-GoRoutine/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	//Go Routine aynı anda birden çok işlemin yapılmasını sağlayan yapıdır.
 
 	go printX()
 	go printY()
-	fmt.Println("Main Bitişi")
+	time.Sleep(time.Second)    //
+	fmt.Println("Main Bitişi") // Çalışmasını bitirmeden main GoRoutini bittiğinden yalnızca çıktı görülmekte.
 }
 
 func printX() {
