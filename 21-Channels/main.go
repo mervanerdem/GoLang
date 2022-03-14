706ffd740feee98b15be616747355042831d7c11
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-15 00:40:25 +0300

    Kanallarla ilgili örnek çözüldü.

--- 21-Channels/main.go
+++ 21-Channels/main.go
@@ -14,7 +14,7 @@ func main() {
 func merhaba(channel chan string) {
 	channel <- "Merhaba" //Buradan ok operatörü ile return yerine fonksiyondan geri dönüş sağladık.
 } */
-
+/*
 //Burada hata alacağız
 package main
 
@@ -26,4 +26,30 @@ func main() {
 	chan1 <- "Merhaba" //değer kanala gönderildi. başka bir go routine gönderilmek adına
 
 	fmt.Println(<-chan1) // fakat başka bir goroutineden geri dönüş olmadığından foksiyon hataya düşmekte.
+} */
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	c1 := circle{5}
+
+	chan1 := make(chan float64)
+
+	go area(c1, chan1)            //fonksiyona c1 ve kanal gönderiliyor.
+	fmt.Println("Alan:", <-chan1) //kanal ile alınan değer yazdırılıyor.
+
+}
+
+type circle struct {
+	r float64
+}
+
+func area(c circle, chan1 chan float64) {
+	result := math.Pi * c.r * c.r
+	chan1 <- result //return yerine bu şekilde <- ok operatörü kullanılmakta.
 }
